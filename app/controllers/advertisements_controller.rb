625d1917f0ef34208cd9a4fc197951850a4a4f8d
Keith Chong <keithchong@donesafe.com>
2017-07-18 19:43:23 +1000

    MVC for advertisements

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,30 @@
+class AdvertisementsController < ApplicationController
+  #before_actions
+
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_advertisement
+  end
+
+  def advertisement_params
+  end
+end
